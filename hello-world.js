4d6d997ff9e708ae45d506d54014516590c79b26
christopherdegroot <christopher.neil.degroot@gmail.com>
2022-04-20 12:21:05 -0700

    add get and listen for hello-world test app

--- hello-world.js
+++ hello-world.js
@@ -4,6 +4,11 @@ const app = express();
 
 const port = 3000;
 
-app.length("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello World, from express");
 });
+
+app.listen(port, () =>
+  console.log(`Hello world app is listening on port ${port}!`)
+);
+
